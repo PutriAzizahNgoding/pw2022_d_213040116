2e06aa7a3e3d4f7de89c80a905026c9387f830fc
PutriAzizahNgoding <myputunity@gmail.com>
2022-06-02 12:20:28 +0700

    mengerjakan sampai pertemuan 13

---
+++ kuliah/pertemuan11/hapus.php
@@ -0,0 +1,11 @@
+<?php 
+require 'functions.php';
+
+$id = $_GET['id'];
+
+if( hapus($id) > 0) {
+    echo "<script>
+            alert('Data berhasil dihapus!');
+            document.location.href = 'kuliah_latihan1.php';
+         </script>";
+}
